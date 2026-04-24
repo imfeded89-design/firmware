d2868c4043857d07ce525d3b962c4a67f16b5dd5
imfeded89-design <imfeded89@gmail.com>
2026-04-24 16:16:25 +0700

    Refactor pin definitions and add TFT/SDCard support

--- boards/ESP-General/pins_arduino.h
+++ boards/ESP-General/pins_arduino.h
@@ -7,63 +7,63 @@
 #define USB_VID 0x303a
 #define USB_PID 0x1001
 
-// Some boards have too low voltage on this pin (board design bug)
-// Use different pin with 3V and connect with 48
-// and change this setup for the chosen pin (for example 38)
+// LED bawaan board
 #define PIN_RGB_LED 48
-// BUILTIN_LED can be used in new Arduino API digitalWrite() like in Blink.ino
 static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
-#define BUILTIN_LED LED_BUILTIN  // backward compatibility
-#define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
-// RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
-#define RGB_BUILTIN    LED_BUILTIN
+#define BUILTIN_LED LED_BUILTIN
+#define LED_BUILTIN LED_BUILTIN
+#define RGB_BUILTIN LED_BUILTIN
 #define RGB_BRIGHTNESS 64
 
+// UART default
 static const uint8_t TX = 43;
 static const uint8_t RX = 44;
 
+// I2C default
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
 
-static const uint8_t SS = 10;
+// SPI shared bus (dipakai semua modul)
 static const uint8_t MOSI = 11;
 static const uint8_t MISO = 13;
-static const uint8_t SCK = 12;
+static const uint8_t SCK  = 12;
 
-static const uint8_t A0 = 1;
-static const uint8_t A1 = 2;
-static const uint8_t A2 = 3;
-static const uint8_t A3 = 4;
-static const uint8_t A4 = 5;
-static const uint8_t A5 = 6;
-static const uint8_t A6 = 7;
-static const uint8_t A7 = 8;
-static const uint8_t A8 = 9;
-static const uint8_t A9 = 10;
-static const uint8_t A10 = 11;
-static const uint8_t A11 = 12;
-static const uint8_t A12 = 13;
-static const uint8_t A13 = 14;
-static const uint8_t A14 = 15;
-static const uint8_t A15 = 16;
-static const uint8_t A16 = 17;
-static const uint8_t A17 = 18;
-static const uint8_t A18 = 19;
-static const uint8_t A19 = 20;
+// --- TFT ST7735 128x160 ---
+#define ST7735_DRIVER 1
+#define TFT_WIDTH 128
+#define TFT_HEIGHT 160
+#define TFT_CS   5
+#define TFT_DC   4
+#define TFT_RST  2
+#define TFT_BL   21
+#define TFT_MOSI MOSI
+#define TFT_SCLK SCK
+#define TFT_MISO MISO
 
-static const uint8_t T1 = 1;
-static const uint8_t T2 = 2;
-static const uint8_t T3 = 3;
-static const uint8_t T4 = 4;
-static const uint8_t T5 = 5;
-static const uint8_t T6 = 6;
-static const uint8_t T7 = 7;
-static const uint8_t T8 = 8;
-static const uint8_t T9 = 9;
-static const uint8_t T10 = 10;
-static const uint8_t T11 = 11;
-static const uint8_t T12 = 12;
-static const uint8_t T13 = 13;
-static const uint8_t T14 = 14;
+// --- SDCard ---
+#define SDCARD_CS   13
+#define SDCARD_MOSI MOSI
+#define SDCARD_SCK  SCK
+#define SDCARD_MISO MISO
+
+// --- CC1101 ---
+#define CC1101_SS_PIN   10
+#define CC1101_GDO0_PIN 9
+#define CC1101_MOSI_PIN MOSI
+#define CC1101_SCK_PIN  SCK
+#define CC1101_MISO_PIN MISO
+
+// --- nRF24 ---
+#define NRF24_SS_PIN   7
+#define NRF24_CE_PIN   6
+#define NRF24_MOSI_PIN MOSI
+#define NRF24_SCK_PIN  SCK
+#define NRF24_MISO_PIN MISO
+
+// --- Tombol fisik ---
+#define BTN_PREV 34   // GPIO untuk tombol Prev
+#define BTN_NEXT 35   // GPIO untuk tombol Next
+#define BTN_SEL  36   // GPIO untuk tombol Select/OK
+#define BTN_ESC  37   // GPIO untuk tombol Exit/Back
 
 #endif /* Pins_Arduino_h */
